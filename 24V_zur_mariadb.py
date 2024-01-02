48fbea925f4db94d8fa5ab1a4efdf20eaca6cc81
FredFeuerstein0815 <110164290+FredFeuerstein0815@users.noreply.github.com>
2024-01-02 07:42:03 +0100

    Create 24V_zur_mariadb.py
    Dieses Script schreibt die Spannung des 24-Volt-Systems in die MariaDB.

---
+++ 24V_zur_mariadb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import mysql.connector
+from mysql.connector import errorcode
+
+SPANNUNG = "/dev/shm/mcp3208_ch5.txt"
+
+def main():
+    datumzeit=(datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S'))
+    try:
+        with open(SPANNUNG, "r") as v:
+            spannung = v.read()
+            db = mysql.connector.connect(user='zerow', password='spannung', host='10.8.0.1', database='spannung24volt')
+            cursor = db.cursor(prepared=True)
+            sql = "INSERT INTO daten (spannung, datumzeit) VALUES (%s, %s)"
+            val = (spannung, datumzeit)
+            cursor.execute(sql,val)
+            db.commit()
+            cursor.close()
+            db.close()
+    except Exception as error:
+        print(error)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Abbruch durch Benutzer\n")
+    finally:
+        print("Bye bye")
